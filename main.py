f0c19f74118c7134be4f49da9728df89330fd7d2
Darwin Acharya <acharyadarwin3@gmail.com>
2025-11-23 17:07:18 +0545

    added expense tracker code

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from fastmcp import FastMCP
 import sqlite3
 import os
 
-
 DB_PATH = os.path.join(os.path.dirname(__file__), "expenses.db")
 CATEGORIES_PATH = os.path.join(os.path.dirname(__file__), "categories.json")
 
@@ -22,26 +21,28 @@ def init_db():
             )   
         """)
 
+
 init_db()
 
 
 @mcp.tool()
 def add_expenses(date, amount, category, subcategory="", note=""):
-    '''Add a new expense entry to the database'''
+    """Add a new expense entry to the database"""
     with sqlite3.connect(DB_PATH) as c:
         cur = c.execute(
             "INSERT INTO expenses(date, amount, category, subcategory, note) VALUES (?,?,?,?,?)",
             (date, amount, category, subcategory, note)
         )
-        return {"status" : "OK", "id" : cur.lastrowid}
+        return {"status": "OK", "id": cur.lastrowid}
 
 
 @mcp.tool()
 def list_expenses(start_date, end_date):
-    '''List all expenses in the database'''
+    """List all expenses within a date range"""
     with sqlite3.connect(DB_PATH) as c:
         cur = c.execute(
-            "SELECT id, date, amount, category, subcategory, note FROM expenses WHERE date BETWEEN ? AND ? ORDER BY id ASC", (start_date, end_date)
+            "SELECT id, date, amount, category, subcategory, note FROM expenses WHERE date BETWEEN ? AND ? ORDER BY id ASC",
+            (start_date, end_date)
         )
         cols = [d[0] for d in cur.description]
         return [dict(zip(cols, row)) for row in cur.fetchall()]
@@ -49,31 +50,30 @@ def list_expenses(start_date, end_date):
 
 @mcp.tool()
 def summarize(start_date, end_date, category=None):
-    '''Summarize expenses in the database'''
+    """Summarize expenses grouped by category"""
     with sqlite3.connect(DB_PATH) as c:
-        query = (
-            """
+        query = """
             SELECT category, SUM(amount) as total_amount
             FROM expenses
             WHERE date BETWEEN ? AND ?
-            """
-        )
+        """
         params = [start_date, end_date]
         if category:
-            query += "AND category = ?"
+            query += " AND category = ?"
             params.append(category)
         
-        query += "GROUP BY category ORDER BY category ASC"
-
+        query += " GROUP BY category ORDER BY category ASC"
+        
         cur = c.execute(query, params)
         cols = [d[0] for d in cur.description]
         return [dict(zip(cols, row)) for row in cur.fetchall()]
 
 
-@mcp.resource("expense://categories", "mine_type=application/json")
+@mcp.resource("expense://categories", "mime_type=application/json")
 def categories():
-    with open(CATEGORIES_PATH, "r", encoding="utf-") as f:
+    with open(CATEGORIES_PATH, "r", encoding="utf-8") as f:
         return f.read()
+
 
 if __name__ == "__main__":
     mcp.run(transport="http", host="0.0.0.0", port=8000)
